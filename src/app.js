8701d7bfaae73a8637591a38c70da603ab4c38c7
kassuro <l.fuhl@demv.de>
2020-07-04 23:21:32 +0200

    basic version of game loop (needs still work)

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const SHAPES = [Rectangle, Circle, Triangle];
 
 let CANVAS;
 let CONTEXT;
+let SHAPE_CORDINATES;
 
 function getRandomNumber(max, min = 1) {
   return Math.floor(Math.random() * max + min);
@@ -34,14 +35,37 @@ function draw() {
   const position = randPosition();
   const shapeWidth = getRandomNumber(100, 10);
   const shapeHeight = getRandomNumber(100, 10);
-  const ShapeConstructor = SHAPES[getRandomNumber(2, 0)];
+  const ShapeConstructor = SHAPES[getRandomNumber(3, 0)];
   const shape = new ShapeConstructor(CONTEXT, position.x, position.y);
   shape.fillStyle = randColor();
   shape.draw(shapeWidth, shapeHeight);
+  SHAPE_CORDINATES = {
+    x: position.x + CANVAS.offsetLeft,
+    y: position.y + CANVAS.offsetTop,
+    x2: position.x + shapeWidth + CANVAS.offsetLeft,
+    y2: position.y + shapeHeight + CANVAS.offsetTop,
+  };
 }
 
 function startGame() {
+  let gameRunning = true;
+  let score = 0;
   draw();
+  CANVAS.addEventListener('click', (event) => {
+    const { x, y } = event;
+    const validXCordinate = x >= SHAPE_CORDINATES.x && x <= SHAPE_CORDINATES.x2;
+    const validYCordinate = y >= SHAPE_CORDINATES.y && y <= SHAPE_CORDINATES.y2;
+
+    if (validXCordinate && validYCordinate) {
+      score += 1;
+      console.log(score);
+      draw();
+    }
+  });
+
+  setTimeout(() => {
+    gameRunning = false;
+  }, 6000);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
