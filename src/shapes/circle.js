7119a84f12c98aedea323af465585a288c5d8147
kassuro <l.fuhl@demv.de>
2020-07-13 20:48:48 +0200

    add click detection method on circle shape class

--- src/shapes/circle.js
+++ src/shapes/circle.js
@@ -2,8 +2,17 @@ import Shape from './shape';
 
 class Circle extends Shape {
   draw(xRadius, yRadius) {
+    this.xRadius = xRadius;
+    this.yRadius = yRadius;
     this.path.ellipse(this.x, this.y, xRadius, yRadius, 0, 0, 2 * Math.PI);
     super.draw();
+  }
+
+  clickedOnShape(x, y) {
+    const xPart = ((x - this.x) ** 2) / this.xRadius ** 2;
+    const yPart = ((y - this.y) ** 2) / this.yRadius ** 2;
+
+    return xPart + yPart <= 1;
   }
 }
 
